2ff50bfc86c47840fc5913a1bc424f7bfe232224
nurulyca <nycovering@gmail.com>
2021-07-16 11:19:10 +0700

    Grid

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name : 'Shoes', description: 'Running shoes.', price: 'Rp2.279.000', image: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a4c3cf11-7f1a-43af-8373-856ee709ce59/air-zoom-vomero-15-running-shoe-wqDgSG.png' },
-    { id: 2, name : 'Macbook', description: 'Apple macbook.', price: 'Rp29.350.775', image: 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//101/MTA-7299263/apple_macbook_air_2020_512gb_full10_f3lxb660.jpg' },
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart } ) => {
     const classes = useStyles();
 
     return (
@@ -18,7 +13,7 @@ const Products = () => {
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
+                    <Product product={product} onAddToCart={onAddToCart}/>
                 </Grid>
             ))}
         </Grid>
